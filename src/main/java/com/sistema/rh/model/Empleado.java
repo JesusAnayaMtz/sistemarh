d0d7cb2fd4ffa3ed4cfe2b4916ebb231f0da8950
Jesus Anaya Mtz <118866969+systemspool@users.noreply.github.com>
2024-08-21 23:26:39 -0600

    Se crea la entidad y se configura el properties
    Se crea la entidad empleado y se configura el properties y se agregan las dependencias que se usaran en el proyecto

---
+++ src/main/java/com/sistema/rh/model/Empleado.java
@@ -0,0 +1,22 @@
+package com.sistema.rh.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "empleados")
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String departamento;
+    private Double sueldo;
+}
